fa1a2b6b304d04ff510a5a3205c18888e25d03eb
cpufilecode <zf0908@naver.com>
2023-10-15 23:31:31 +0900

    upload

---
+++ src/ch02/FloatVsDouble.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class FloatVsDouble {
+    public static void main(String[] args) {
+        float f1 = 1.0000001f;
+        System.out.println(f1);
+        float f2 = 1.00000001f;
+        System.out.println(f2);
+
+        double d1 = 1.000000000000001;
+        System.out.println(d1);
+
+        double d2 = 1.0000000000000001;
+        System.out.println(d2);
+    }
+}
