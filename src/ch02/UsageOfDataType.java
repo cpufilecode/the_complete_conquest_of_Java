bc026916a03e72ba81297b5da753d93c5ad822a2
cpufilecode <zf0908@naver.com>
2023-10-15 23:02:12 +0900

    upload

---
+++ src/ch02/UsageOfDataType.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class UsageOfDataType {
+    public static void main(String[] args) {
+        int a =  3;
+        int b;
+        b = 4;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
