a356d9860e53d339f82dc63d9169af7b0f7e8976
cpufilecode <zf0908@naver.com>
2023-10-21 23:15:17 +0900

    upload

---
+++ src/ch03/LogicalOperator.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(true && (5<3));
+        System.out.println((5<=5) && (7>2));
+        System.out.println();
+    }
+}
