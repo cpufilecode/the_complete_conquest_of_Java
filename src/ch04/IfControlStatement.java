f4945cefc2dfa8a496ddeec00057f4c46b97ff75
cpufilecode <zf0908@naver.com>
2023-10-26 12:00:35 +0900

    upload

--- src/ch04/IfControlStatement.java
+++ src/ch04/IfControlStatement.java
@@ -3,6 +3,22 @@ package ch04;
 public class IfControlStatement {
     public static void main(String[] args) {
         int value1 = 5;
-        
+        if(value1 > 3) {
+            System.out.println("실행1");
+        }
+        if(value1 < 5) {
+            System.out.println("실행2");
+        }
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+        if(bool1) {
+            System.out.println("실행3");
+        }
+        if(bool2) {
+            System.out.println("실행4");
+        }
+
+
     }
 }
