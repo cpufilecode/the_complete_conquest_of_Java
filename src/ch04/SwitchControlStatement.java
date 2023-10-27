aad8ddad8c40edbebf358c8bc81f24145792f116
cpufilecode <zf0908@naver.com>
2023-10-27 22:39:46 +0900

    upload

--- src/ch04/SwitchControlStatement.java
+++ src/ch04/SwitchControlStatement.java
@@ -2,6 +2,22 @@ package ch04;
 
 public class SwitchControlStatement {
     public static void main(String[] args) {
+        int value1 = 2;
+        switch (value1) {
+            case 1:
+                System.out.println("A");
+            case 2:
+                System.out.println("B");
+            case 3:
+                System.out.println("C");
+            default:
+                System.out.println("D");
+        }
+        System.out.println();
 
+        int value2 = 2;
+        switch (value2) {
+
+        }
     }
 }
